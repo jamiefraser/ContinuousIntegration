d644afdef648a8ff99e9f56f05023dcf992d53cd
jfraser <jamie.fraser@imason.com>
2016-08-24 16:22:49 -0400

    Trivial UI.  Tested on sim

--- ContinuousIntegration.iOS/Views/FirstView.cs
+++ ContinuousIntegration.iOS/Views/FirstView.cs
@@ -2,13 +2,17 @@ using MvvmCross.iOS.Views;
 using System;
 
 using UIKit;
-
+using Cirrious.FluentLayouts.Touch;
 namespace ContinuousIntegration.iOS.Views
 {
     public partial class FirstView : MvxViewController
     {
+        UILabel labelHello;
         public FirstView() : base("FirstView", null)
         {
+            labelHello = new UILabel();
+            labelHello.Text = "Hello!";
+            
         }
 
         public override void DidReceiveMemoryWarning()
@@ -21,6 +25,13 @@ namespace ContinuousIntegration.iOS.Views
         public override void ViewDidLoad()
         {
             base.ViewDidLoad();
+            View.Add(labelHello);
+
+            View.SubviewsDoNotTranslateAutoresizingMaskIntoConstraints();
+            this.View.AddConstraints(labelHello.WithSameCenterX(this.View)
+                                     ,labelHello.WithSameCenterY(this.View)
+                         //, OKButton.WithSameCenterX(this.View)
+                         );
 
             // Perform any additional setup after loading the view, typically from a nib.
         }
